5b3241bea3bb3a2e523b162ea1f597ac43f5a81d
Elisabete Costa <elisabete_costa94@hotmail.com>
2024-12-02 15:14:09 +0000

    primeiro programa em c++

---
+++ ProgCPP_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Usa o namespace "std" por defeito, não sendo necessário colocar std:: antes das funções
+using namespace std; 
+
+int main() {
+
+    int number;
+    cout << "Introduza um número: "; // output
+    cin >> number; // input
+    cout << "O número introduzido foi: " << number << endl;
+    return 0;
+}
