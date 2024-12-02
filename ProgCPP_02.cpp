6caec7720e0bbdab7bc56490af9721cc294bd4b6
Elisabete Costa <elisabete_costa94@hotmail.com>
2024-12-02 16:53:25 +0000

    segundo programa em C++

---
+++ ProgCPP_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+
+    int age;
+    float height;
+    cout << "Introduza a sua idade e a seguir a sua altura em cm: ";
+    cin >> age >> height;
+    cout << "Você tem " << age << " anos e " << height << " cm." << endl;
+    return 0;
+}
